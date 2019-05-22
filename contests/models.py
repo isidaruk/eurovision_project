9d75b0cec69551f13321956780bc2c4d466c5383
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-05-22 18:59:19 +0300

    Fixed Contest model

--- contests/models.py
+++ contests/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
 
 from countries.models import Country
 
 
 class Contest(models.Model):
-    year = models.IntegerField(unique=True)
+    year = models.IntegerField(blank=False, validators=[MaxValueValidator(timezone.now().year), MinValueValidator(1956)], unique=True)
     host_country = models.ForeignKey(
         Country,
         on_delete=models.CASCADE,
