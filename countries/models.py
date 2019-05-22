773e730354b2e9c325083cfd20fd36b3f9473d9a
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-05-22 15:25:28 +0300

    Register Country model with admin

--- countries/models.py
+++ countries/models.py
@@ -4,5 +4,9 @@ from django.db import models
 class Country(models.Model):
     name = models.CharField(max_length=56, unique=True)
 
+    class Meta:
+        verbose_name = 'country'
+        verbose_name_plural = 'countries'
+
     def __str__(self):
         return self.name
