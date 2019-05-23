36632fdc5947900eb982a52d17238492f1bd9bfd
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-05-23 15:20:56 +0300

    Updated model, run migrations

--- votes/models.py
+++ votes/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 from countries.models import Country
 
 
 class Vote(models.Model):
-    point = models.PositiveIntegerField(default=0)
+    point = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(12)])
 
     from_country = models.ForeignKey(
         Country,
@@ -16,3 +17,6 @@ class Vote(models.Model):
         on_delete=models.CASCADE,
         related_name='to_votes',
     )
+
+    def __str__(self):
+        return '{} points from {} to {}'.format(self.point, self.from_country.name, self.to_country.name)
