1368955221a0a51d1bd4fb41a5189c3b166d2624
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-05-24 12:02:42 +0300

    Installed drf-yasg

--- eurovision_project/settings.py
+++ eurovision_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'votes.apps.VotesConfig',
 
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- eurovision_project/urls.py
+++ eurovision_project/urls.py
@@ -14,12 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
-from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
-schema_view = get_schema_view(title='Eurovision API')
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Eurovision API",
+        default_version='v0',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 api_version = 'v0/'
 api_url = 'api/' + api_version
@@ -30,7 +39,10 @@ api_urlpatterns = [
     path('votes/', include('votes.api.urls')),
     path('contests/', include('contests.api.urls')),
     path('countries/', include('countries.api.urls')),
-    path('schema/', schema_view),
+    re_path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 
 urlpatterns = [
