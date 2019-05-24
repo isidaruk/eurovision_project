1eeb82f65d5ed7810d0242f54b6233df8857cc36
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-05-24 18:23:41 +0300

    Enable POST for Votes

--- votes/api/serializers.py
+++ votes/api/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
 from votes.models import Vote
+from countries.models import Country
+from participants.models import Participant
 
 
 class VoteSerializer(serializers.HyperlinkedModelSerializer):
+    from_country = serializers.PrimaryKeyRelatedField(queryset=Country.objects.all())
+    to_country = serializers.PrimaryKeyRelatedField(queryset=Participant.objects.all())
 
     class Meta:
         model = Vote
