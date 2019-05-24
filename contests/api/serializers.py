e9e0efad3ebe11a2fac99646397b35348206dc3e
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-05-24 18:31:32 +0300

    Fixed queryset in serializers

--- contests/api/serializers.py
+++ contests/api/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
 from contests.models import Contest
+from countries.models import Country
 
 
 class ContestSerializer(serializers.HyperlinkedModelSerializer):
-    host_country = serializers.PrimaryKeyRelatedField(queryset=Contest.objects.all())
+    host_country = serializers.PrimaryKeyRelatedField(queryset=Country.objects.all())
 
     class Meta:
         model = Contest
