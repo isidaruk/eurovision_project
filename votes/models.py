0c556c191f5333aaa43c8f9930501c719fb12716
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-05-24 10:37:54 +0300

    Changed Votes model

--- votes/models.py
+++ votes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 
 from countries.models import Country
+from participants.models import Participant
 
 
 class Vote(models.Model):
@@ -13,10 +14,12 @@ class Vote(models.Model):
         related_name='from_votes',
     )
     to_country = models.ForeignKey(
-        Country,
+        Participant,
         on_delete=models.CASCADE,
         related_name='to_votes',
     )
 
     def __str__(self):
-        return '{} points from {} to {}'.format(self.point, self.from_country.name, self.to_country.name)
+        return '{} points from {} to {} ({})'.format(
+            self.point, self.from_country.name, self.to_country.artist_id.name, self.to_country.country_id.name
+        )
