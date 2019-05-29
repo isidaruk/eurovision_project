68ff120ebfc20ebf9cca4be82caf71c7bae7d10b
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-05-29 18:02:11 +0300

    Put logic inside of try block

--- participants/management/commands/loadparticipants.py
+++ participants/management/commands/loadparticipants.py
@@ -17,77 +17,49 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         csv_filename = options['csv_filename']
 
-        # try:
-        #     with open(f'{csv_filename}', 'r') as csv_file:
-        #         csv_reader = csv.reader(csv_file)
+        try:
+            with open(f'{csv_filename}', 'r') as csv_file:
+                csv_reader = csv.reader(csv_file)
 
-        #         next(csv_reader)  # The first linie is the header, loop over the first line.
+                next(csv_reader)  # The first linie is the header, loop over the first line.
 
-        #         for artist_id, contest_id, country_id, song in csv_reader:
-        #             # Check if there is any data:
-        #             if not (artist_id and contest_id and country_id and song):
-        #                 self.stdout.write(self.style.WARNING("There are some data missing."))
-        #             else:
-        #                 # If there is Contest, we should check if the Country for this year is not already in db.
-        #                 if Contest.objects.filter(id=contest_id).exists():
+                for artist_id, contest_id, country_id, song in csv_reader:
+                    # Check if there is any data:
+                    if not (artist_id and contest_id and country_id and song):
+                        self.stdout.write(self.style.WARNING(f"There are some data missing {artist_id, contest_id, country_id, song}."))
 
-        #                     pass
+                    else:
+                        # If there is Contest, we should check if there is a Country_ID and Artist_ID in db.
+                        if Contest.objects.filter(id=contest_id).exists():
 
-        #                     if Country.objects.filter(id=country_id).exists():
+                            # Maybe the better way to check it separetely.
+                            if Country.objects.filter(id=country_id).exists() and Artist.objects.filter(id=artist_id).exists():
+                                artist = Artist.objects.get(id=artist_id)
+                                contest = Contest.objects.get(id=contest_id)
+                                country = Country.objects.get(id=country_id)
 
-        #                         # If there is an Artist with these ID:
-        #                         p = Participant(artist=Artist.objects.get(id=artist_id),
-        #                                         contest=Contest.objects.get(id=contest_id),
-        #                                         country=Country.objects.get(id=country_id),
-        #                                         song=song
-        #                                         )
-        #                         p.save()
+                                if Participant.objects.all().filter(artist=artist_id, contest=contest_id, country=country_id):
+                                    self.stdout.write(self.style.WARNING(f"This record ({artist.name} - {song} - {country.name} - {contest.year}) already in db. Skipped."))
 
-        #                     else:
-        #                         self.stdout.write(self.style.WARNING("Duplicated data are not allowed."))
+                                else:
+                                    # Save if validation was passed.
+                                    p = Participant(artist=artist, contest=contest, country=country, song=song)
+                                    p.save()
 
-        # except Exception as e:
-        #     raise CommandError(f"File '{csv_filename}' does not exist.")
-
-        # self.stdout.write(self.style.SUCCESS(
-        #     f"Done. Successfully loaded data from '{csv_filename}'."))
-
-        with open(f'{csv_filename}', 'r') as csv_file:
-            csv_reader = csv.reader(csv_file)
-
-            next(csv_reader)  # The first linie is the header, loop over the first line.
-
-            for artist_id, contest_id, country_id, song in csv_reader:
-                # Check if there is any data:
-                if not (artist_id and contest_id and country_id and song):
-                    self.stdout.write(self.style.WARNING(f"There are some data missing {artist_id, contest_id, country_id, song}."))
-
-                else:
-                    # If there is Contest, we should check if there is a Country_ID and Artist_ID in db.
-                    if Contest.objects.filter(id=contest_id).exists():
-
-                        # Maybe the better way to check it separetely.
-                        if Country.objects.filter(id=country_id).exists() and Artist.objects.filter(id=artist_id).exists():
-                            artist = Artist.objects.get(id=artist_id)
-                            contest = Contest.objects.get(id=contest_id)
-                            country = Country.objects.get(id=country_id)
-
-                            if Participant.objects.all().filter(artist=artist_id, contest=contest_id, country=country_id):
-                                self.stdout.write(self.style.WARNING(f"This record ({artist.name} - {song} - {country.name} - {contest.year}) already in db. Skipped."))
+                                    self.stdout.write(self.style.SUCCESS(
+                                        f"Data ({artist.name} - {song} - {country.name} - {contest.year}) was successfully saved to the Particpant table in database.")
+                                    )
 
                             else:
-                                # Save if validation was passed.
-                                p = Participant(artist=artist, contest=contest, country=country, song=song)
-                                p.save()
-
-                                self.stdout.write(self.style.SUCCESS(
-                                    f"Data ({artist.name} - {song} - {country.name} - {contest.year}) was successfully saved to the Particpant table in database.")
+                                self.stdout.write(
+                                    self.style.WARNING(f"The Country or Artist with the IDs {country_id, artist_id} respectively do not exists in database. Skipped.")
                                 )
 
                         else:
-                            self.stdout.write(
-                                self.style.WARNING(f"The Country or Artist with the IDs {country_id, artist_id} respectively do not exists in database. Skipped.")
-                            )
+                            self.stdout.write(self.style.WARNING(f"The Contest with the ID {contest_id} does not exists in database. Skipped."))
 
-                    else:
-                        self.stdout.write(self.style.WARNING(f"The Contest with the ID {contest_id} does not exists in database. Skipped."))
+        except Exception as e:
+            raise CommandError(f"File '{csv_filename}' does not exist.")
+
+        self.stdout.write(self.style.SUCCESS(
+            f"Done. Successfully loaded data from '{csv_filename}'."))
