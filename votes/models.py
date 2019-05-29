e45b5223d2856c737ac984acede6027ceadf00b9
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-05-29 10:21:40 +0300

    Changed name of field in Vote table

--- votes/models.py
+++ votes/models.py
@@ -9,9 +9,9 @@ class Vote(models.Model):
     point = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(12)], unique=True)
 
     from_voter = models.ForeignKey(Voter, on_delete=models.CASCADE, default=None)
-    to_country = models.ForeignKey(Participant, on_delete=models.CASCADE,)
+    to_participant = models.ForeignKey(Participant, on_delete=models.CASCADE,)
 
     def __str__(self):
         return '{} points from {} ({}) to {} - {}'.format(
-            self.point, self.from_voter.country, self.from_voter.vote_key, self.to_country.artist.name, self.to_country.country.name
+            self.point, self.from_voter.country, self.from_voter.vote_key, self.to_participant.artist.name, self.to_participant.country.name
         )
