e35da34ef84b28c5bd77e78b144b224d1002317f
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-03 14:36:44 +0300

    Updated views with fixed business logic

---
+++ votes/api/services.py
@@ -0,0 +1,50 @@
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseBadRequest, HttpResponseForbidden
+from votes.models import Vote
+from voters.models import Voter
+from participants.models import Participant
+
+
+def check_voters(validated_data, sended_vote_key):
+    errors = []
+
+    vote_point = validated_data.get('point')
+    from_voter = validated_data.get('from_voter')
+    to_participant = validated_data.get('to_participant')
+
+    voter = Voter.objects.get(id=from_voter.id)
+    participant = Participant.objects.get(id=to_participant.id)
+
+    # -*- Check Cases -*-
+
+    # If voter have Token.
+    if from_voter and sended_vote_key:
+
+        if str(from_voter.vote_key) != str(sended_vote_key):
+            # return HttpResponseForbidden(content='You are not allowed to vote this year.')
+            errors.append('You are not allowed to vote this year.')
+        else:
+
+            # Count how many times voter has voted.
+            if Vote.objects.filter(from_voter=from_voter.id).count() == 10:
+                # raise PermissionDenied()
+                # return HttpResponseBadRequest(content="You've voted 10 times.")
+                errors.append("You've voted 10 times.")
+            else:
+
+                # If the point is not used.
+                if Vote.objects.filter(from_voter=from_voter.id).filter(point__iexact=vote_point).count() != 0:  # ?
+                    # return HttpResponseBadRequest(content=f"{vote_point} point is already given.")
+                    errors.append(f"{vote_point} point is already given.")
+
+                # Voting for your own country.
+                if voter.country.name == participant.country.name:
+                    # return HttpResponseBadRequest(content='You are not allowed to vote for your own country.')
+                    errors.append('You are not allowed to vote for your own country.')
+
+                # Check vote for the same country.
+                if Vote.objects.filter(to_participant=to_participant.id).exists():
+                    # return HttpResponseBadRequest(content="You've already given a point to these participant.")
+                    errors.append("You've already given a point to these participant.")
+
+    return errors
--- votes/api/views.py
+++ votes/api/views.py
@@ -1,28 +1,12 @@
-# from rest_framework import viewsets
-
-# from votes.models import Vote
-# from votes.api.serializers import VoteSerializer
-
-
-# class VoteViewSet(viewsets.ModelViewSet):
-#     queryset = Vote.objects.all()
-#     serializer_class = VoteSerializer
-
-
-from votes.models import Vote
-from votes.api.serializers import VoteSerializer
-from django.core.exceptions import PermissionDenied
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
-# Should be moved to the top.
-from django.http import HttpResponseBadRequest, HttpResponseForbidden
 from votes.models import Vote
-from voters.models import Voter
-from participants.models import Participant
+from votes.api.serializers import VoteSerializer
+from votes.api.services import check_voters
 
 
 # class VoteList(APIView):
@@ -35,7 +19,7 @@ class VoteList(GenericAPIView):
     serializer_class = VoteSerializer
 
     def get(self, request, format=None):
-        print(request)
+        # print(request)
         votes = Vote.objects.all()
         serializer = VoteSerializer(votes, many=True, context={'request': request})
         return Response(serializer.data)
@@ -48,43 +32,13 @@ class VoteList(GenericAPIView):
             # Custom Token validation.
             sended_vote_key = request.headers.get('Token')
 
-            from_voter = serializer.validated_data.get('from_voter')
+            errors = check_voters(serializer.validated_data, sended_vote_key)
 
-            # -*- Check Cases -*-
-
-            to_participant = serializer.validated_data.get('to_participant')
-
-            vote_point = serializer.validated_data.get('point')
-
-            # print(from_voter, type(from_voter))
-            # print(to_participant, type(to_participant))
-
-            voter = Voter.objects.get(id=from_voter.id)
-            participant = Participant.objects.get(id=to_participant.id)
-
-            # If voter have Token.
-            if from_voter and sended_vote_key:
-                if str(from_voter.vote_key) != str(sended_vote_key):
-                    return HttpResponseForbidden(content='You are not allowed to vote this year.')
-
-                # Count how many times voter has voted.
-                if Vote.objects.filter(from_voter=from_voter.id).count() == 10:
-                    return HttpResponseBadRequest(content="You've voted 10 times.")
-
-                # If the point is not used.
-                if Vote.objects.filter(from_voter=from_voter.id).filter(point__iexact=vote_point).count() != 0:  # ?
-                    return HttpResponseBadRequest(content=f"{vote_point} point is already given.")
-
-                # Voting for your own country.
-                if voter.country.name == participant.country.name:
-                    return HttpResponseBadRequest(content='You are not allowed to vote for your own country.')
-
-                # Check vote for the same country.
-                if not Vote.objects.filter(to_participant=to_participant.id).exists():
-                    print('Your vote was accepted.')
-                    serializer.save()
-                else:
-                    return HttpResponseBadRequest(content="You've already given a point to these participant.")
+            if errors:
+                return Response(data=errors, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                # Vote was accepted.
+                serializer.save()
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
