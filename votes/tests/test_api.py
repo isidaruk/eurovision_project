726c22f2a8fc0b261de9fc2af03014817c3a44a5
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-04 12:03:27 +0300

    Draft test

--- votes/tests/test_api.py
+++ votes/tests/test_api.py
@@ -16,7 +16,7 @@ import requests
 
 server_rsp_params = [
     ('/api/v0/', 200),
-    ('/api/v0/votes/r', 403),
+    ('/api/v0/votes/', 403),
     ('/fake-url', 404)
 ]
 
@@ -26,3 +26,35 @@ def test_server_responses(url, status_code):
     base_url = 'http://127.0.0.1:8000'
     rsp = requests.get(base_url + url)
     assert rsp.status_code == status_code
+
+
+# #  Test Votes API
+# def test_post_response_200():
+#     # test
+#     pass
+
+
+endpoint = 'http://17.0.0.1:8000/api/v0/votes/'
+
+
+vote_data = [
+    (4, 66, 12),  # "12 poin is already given"
+]
+
+
+@pytest.mark.parametrize('from_voter, to_participant', 'point', vote_data)
+#
+@pytest.mark.smoketest
+def test_vote_post_endpoint():
+    """
+    Verifies that the post vote endpoint is returning data with an expected
+    """
+
+    resp = requests.post(endpoint, {
+        "from_voter": "4",
+        "to_participant": "66",
+        "point": "12"
+    })
+
+    assert resp.status_code == 200
+    assert schema.is_valid(resp.json())
