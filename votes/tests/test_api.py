0831e61ebab20dff21d4e7c221d93a7bb7bebfc9
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-06 13:52:02 +0300

    Bad hardcoded version of tests. Covered all cases with

--- votes/tests/test_api.py
+++ votes/tests/test_api.py
@@ -13,20 +13,30 @@ TOKEN = "c8566b58-9d83-4086-9071-64c227e6e236"
 
 test_data = [
     # 4 - Norway, 64 - Belgium
-    (4, 64, 10, 403, "c8566b58-9d83-4086-9071-64c227e6e236XXX"),  # test invalid Token -- 403 Forbidden b'{"detail":"You do not have permission to perform this action."}'
-    (4, 64, 10, 201, TOKEN),  # test success -- 201 Created b'{"id":102,"from_voter":4,"to_participant":64,"point":10}'
+    (4, 64, 10, 403, "c8566b58-9d83-4086-9071-64c227e6e236XXX"),  # test invalid Token (permission errors) -- 403 Forbidden b'{"detail":"You do not have permission to perform this action."}'
+    (4, 64, 10, 201, TOKEN),  # test success (successful casses)-- 201 Created b'{"id":102,"from_voter":4,"to_participant":64,"point":10}'
+
 
     # 4 - Norway, 66 - Slovenia
-    (4, 66, 10, 400, TOKEN),  # test failed -- 400 Bad Request b'["10 point is already given."]'
+    (4, 66, 10, 400, TOKEN),  # test failed (validation errors)-- 400 Bad Request b'["10 point is already given."]'
 
     # 4 - Norway, 3 - Norway
-    (4, 3, 8, 400, TOKEN),  # test failed -- 400 Bad Request b'["You are not allowed to vote for your own country."]'
+    (4, 3, 8, 400, TOKEN),  # test failed (validation errors) -- 400 Bad Request b'["You are not allowed to vote for your own country."]'
 
     # 4 - Norway, 64 - Belgium
-    (4, 64, 8, 400, TOKEN),  # test failed -- 400 Bad Request b'["You\'ve already given a point to these participant."]'
+    (4, 64, 8, 400, TOKEN),  # test failed (validation errors) -- 400 Bad Request b'["You\'ve already given a point to these participant."]'
 
+    # Successful
     # 4 - Norway, 66 - Slovenia
-    (4, 66, 8, 201, TOKEN),  # test success -- 201 Created b'{"id":102,"from_voter":4,"to_participant":66,"point":8}'
+    (4, 66, 8, 201, TOKEN),  # test success (successful casses) -- 201 Created b'{"id":X,"from_voter":4,"to_participant":66,"point":8}'
+
+    # 4 - Norway, 67 - Russia
+    (4, 67, 12, 201, TOKEN),  # test success (successful casses) -- 201 Created b'{"id":X,"from_voter":4,"to_participant":67,"point":12}'
+
+
+    # Votes exhusted.
+    # 4 - Norway, 67 - Russia
+    (4, 67, 12, 400, TOKEN),  # test failed (validation errors) -- 400 Bad Request b'["You've voted 10 times"]'
 
 ]
 
