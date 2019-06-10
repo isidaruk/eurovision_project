57c59f27fed3e4f4d0498129da4dcc7da26b82ad
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-10 17:59:23 +0300

    Celery settings

---
+++ eurovision_project/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+# import celery
+from .celery import app as celery_app
--- eurovision_project/settings.py
+++ eurovision_project/settings.py
@@ -135,3 +135,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+# CELERY STUFF
+BROKER_URL = 'amqp://guest:guest@localhost:5672/'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
