cf01d03807a41f2dbede6efdbb725cddd4a48970
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-11 11:51:24 +0300

    Added test tasks to votes

---
+++ votes/tasks.py
@@ -0,0 +1,25 @@
+# from .celery import app as celery_app
+from __future__ import absolute_import, unicode_literals
+
+from celery import shared_task
+from votes.models import Vote
+
+
+@shared_task
+def add(x, y):
+    return x + y
+
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
+
+
+@shared_task
+def count_votes():
+    return Vote.objects.count()
