975df138a7eae3b9970ea804d38f9b19cd719ad2
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-11 15:41:35 +0300

    Fixed bad solution for cicular imports

--- votes/tasks.py
+++ votes/tasks.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
 from django.db.models import Sum
+from django.apps import apps
 
 
 @shared_task
@@ -28,7 +29,9 @@ def xsum(numbers):
 
 @shared_task
 def recalculate_total_votes_for_participant(to_participant_id):
-    from votes.models import Vote
+    # from votes.models import Vote
+    # from django.apps import apps
+    Vote = apps.get_model('votes.Vote')
     total = Vote.objects.filter(to_participant=to_participant_id).aggregate(Sum('point'))
 
     return total['point__sum']
