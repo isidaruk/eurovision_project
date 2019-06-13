3dd847c820991d280f0c1412edb53ff81a56b3f2
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-13 13:53:50 +0300

    Added test for returned content

--- votes/tests/tests.py
+++ votes/tests/tests.py
@@ -3,6 +3,9 @@
 import pytest
 from .factories import CountryFactory
 
+import json
+from .factories import ContestFactory
+
 
 # 1
 # test countries response get's a 200 code:
@@ -11,3 +14,15 @@ def test_countries_endpoint(client):
     new_country = CountryFactory()
     response = client.get(f'/api/v0/countries/{new_country.id}', follow=True)
     assert response.status_code == 200
+
+
+# 2
+# test contests response get's a 200 code, and the host_coountry and the year api returned is the we actually want:
+@pytest.mark.django_db
+def test_contests_endpoint(client):
+    new_contest = ContestFactory()
+    response = client.get(f'/api/v0/contests/{new_contest.id}', follow=True)
+    content = json.loads(response.content)
+    assert response.status_code == 200
+    assert content['host_country'] == new_contest.host_country.id
+    assert content['year'] == new_contest.year
