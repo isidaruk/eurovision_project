39301ed4652079444dfee92e91ac581656aded44
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-13 17:23:57 +0300

    Deleted comments

--- votes/tests/factories.py
+++ votes/tests/factories.py
@@ -1,5 +1,4 @@
 import factory
-# from faker import Factory
 
 from artists.models import Artist
 from contests.models import Contest
@@ -10,15 +9,10 @@ from voters.models import Voter
 from votes.models import Vote
 
 
-# fake = Factory.create()
-
-
 class ArtistFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Artist
 
-    # fields
-    # name = fake.word()
     name = factory.Sequence(lambda n: 'Artist {}'.format(n))
 
 
@@ -26,8 +20,6 @@ class CountryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Country
 
-    # fields
-    # name = fake.word()
     name = factory.Sequence(lambda n: 'Country {}'.format(n))
 
 
@@ -35,7 +27,6 @@ class ContestFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Contest
 
-    # fields
     year = factory.Sequence(lambda n: 1956 + n)
     host_country = factory.SubFactory(CountryFactory)
 
@@ -44,13 +35,11 @@ class ParticipantFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Participant
 
-    # fields
     artist = factory.SubFactory(ArtistFactory)
     country = factory.SubFactory(CountryFactory)
 
-    # song = fake.word()
     song = factory.Faker('word')
-    # song = factory.Sequence(lambda n: 'Song {}'.format(n))
+
     contest = factory.SubFactory(ContestFactory)
 
 
@@ -58,7 +47,6 @@ class VoterFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Voter
 
-    # fields
     country = factory.SubFactory(CountryFactory)
     contest = factory.SubFactory(ContestFactory)
     vote_key = factory.Sequence(lambda n: n)
@@ -68,8 +56,7 @@ class VoteFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Vote
 
-    # fields
     from_voter = factory.SubFactory(VoterFactory)
     to_participant = factory.SubFactory(ParticipantFactory)
-    # point = 1
+
     point = factory.Iterator([1, 2, 3, 4, 5, 6, 7, 8, 10, 12])
