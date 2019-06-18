2a1f176087ed7d38c32ce2af8b65842f7f609e30
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-18 11:18:54 +0300

    Deleted comments and unused imports

--- participants/api/views.py
+++ participants/api/views.py
@@ -1,14 +1,3 @@
-# from rest_framework import viewsets
-
-# from participants.models import Participant
-# from participants.api.serializers import ParticipantSerializer
-
-
-# class ParticipantViewSet(viewsets.ModelViewSet):
-#     queryset = Participant.objects.all()
-#     serializer_class = ParticipantSerializer
-
-
 from participants.models import Participant
 from participants.api.serializers import ParticipantSerializer
 from django.http import Http404
@@ -17,16 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from django.core.cache import cache
 
-# from django.conf import settings
-# from django.core.cache.backends.base import DEFAULT_TIMEOUT
-
-
-# CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
-
-
-# class ParticipantList(APIView):
 class ParticipantList(GenericAPIView):
     """
     List all participants, or create a new participant.
