6fc59dea4dd2243a79741effe551380fba7bde05
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-20 19:01:17 +0300

    Refactored celery

--- eurovision_project/celery.py
+++ eurovision_project/celery.py
@@ -4,16 +4,14 @@ from celery import Celery
 from django.conf import settings
 
 
-# set the default Django settings module for the 'celery' program.
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eurovision_project.settings')
 
 app = Celery('eurovision_project')
 
-
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.autodiscover_tasks()
 
 
