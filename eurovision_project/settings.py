d097442238adc188bc4b5f7ac502e238717a0fb9
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-21 10:52:07 +0300

    Deleted comment, changed setting for debug toolbar

--- eurovision_project/settings.py
+++ eurovision_project/settings.py
@@ -173,8 +173,6 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-# CELERY_TIMEZONE = 'Europe/Minsk'
-# CELERY_ENABLE_UTC = True
 
 
 CACHES = {
@@ -212,5 +210,5 @@ if DEBUG:
     ]
 
     DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False,
+        'DISABLE_PANELS': [],
     }
