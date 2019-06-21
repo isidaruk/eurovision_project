30323d1de84f0025a5bc1b00c5bd6779f9ecc909
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-21 10:51:30 +0300

    Fixed blank lines

--- eurovision_project/api.py
+++ eurovision_project/api.py
@@ -3,13 +3,12 @@ from rest_framework import routers
 from artists.api.views import ArtistViewSet
 from contests.api.views import ContestViewSet
 from countries.api.views import CountryViewSet
-
 from voters.api.views import VoterViewSet
 
 
 router = routers.DefaultRouter()
+
 router.register('artists', ArtistViewSet)
 router.register('contests', ContestViewSet)
 router.register('countries', CountryViewSet)
-
 router.register('voters', VoterViewSet)
