437bd32612e1a14132bb3ecf8773e2c6bdea3472
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-25 11:33:06 +0300

    Fixed invalid escape sequence

--- eurovision_project/urls.py
+++ eurovision_project/urls.py
@@ -44,7 +44,7 @@ api_urlpatterns = [
     path('votes/', include('votes.api.urls')),
     path('contests/', include('contests.api.urls')),
     path('countries/', include('countries.api.urls')),
-    re_path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('voters/', include('voters.api.urls')),
