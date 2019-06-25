8e5d699499a126f93bd98fa723809af845e39823
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-25 17:07:56 +0300

    Added logging to task

--- votes/tasks.py
+++ votes/tasks.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
+import logging
+
 from django.apps import apps
 from django.db.models import Sum
 from django.conf import settings
@@ -10,6 +12,8 @@ from celery import shared_task
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
+logger = logging.getLogger('vote')
+
 
 @shared_task
 def recalculate_total_votes_for_participant(to_participant_id):
@@ -18,6 +22,6 @@ def recalculate_total_votes_for_participant(to_participant_id):
     total_score = total['point__sum']
 
     cache.set(to_participant_id, total_score, CACHE_TTL)
-    print(f'Score for participant {to_participant_id}: {total_score}. Caching...')
+    logger.info(f'Score for participant {to_participant_id}: {total_score}. Caching...')
 
     return total_score
