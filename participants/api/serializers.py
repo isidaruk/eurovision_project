49d2abe831a5e8ff3207f30cdfdf574cad228eb0
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-25 17:11:51 +0300

    Changed to only read total_score from cache

--- participants/api/serializers.py
+++ participants/api/serializers.py
@@ -8,12 +8,6 @@ from countries.models import Country
 
 from django.core.cache import cache
 
-from django.conf import settings
-from django.core.cache.backends.base import DEFAULT_TIMEOUT
-
-
-CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
-
 
 class ParticipantSerializer(serializers.ModelSerializer):
     artist = serializers.PrimaryKeyRelatedField(queryset=Artist.objects.all())
@@ -28,14 +22,6 @@ class ParticipantSerializer(serializers.ModelSerializer):
         depth = 1
 
     def get_total_score(self, obj):
+        """Get total score for this Participant from cache, or return None."""
 
-        # Get total score for this Participant from cache, or recalculate it.
-        total_score_cached = cache.get(obj.pk, default=None)
-
-        if total_score_cached is None:
-            total_score_data = obj.total_score
-            cache.set(obj.pk, total_score_data, CACHE_TTL)
-            print(f'Score for participant {obj.pk}: {total_score_data}. Caching...')
-            return total_score_data
-
-        return total_score_cached
+        return cache.get(obj.pk, default=None)
