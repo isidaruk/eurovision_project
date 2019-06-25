5616d5746852103a3cf12d0b6b2600647a59cbda
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-25 17:04:20 +0300

    Added caching in celery task

--- votes/tasks.py
+++ votes/tasks.py
@@ -1,9 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 
+from django.apps import apps
+from django.db.models import Sum
+from django.conf import settings
+from django.core.cache import cache
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+
 from celery import shared_task
 
-from django.db.models import Sum
-from django.apps import apps
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
 @shared_task
@@ -11,5 +16,8 @@ def recalculate_total_votes_for_participant(to_participant_id):
     Vote = apps.get_model('votes.Vote')
     total = Vote.objects.filter(to_participant=to_participant_id).aggregate(Sum('point'))
     total_score = total['point__sum']
+
+    cache.set(to_participant_id, total_score, CACHE_TTL)
+    print(f'Score for participant {to_participant_id}: {total_score}. Caching...')
 
     return total_score
