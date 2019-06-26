263ea7a3d3da60103a29db6a854f67ff5849802c
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-26 16:27:47 +0300

    Rewrite loadcontests with helper function

--- contests/management/commands/loadcontests.py
+++ contests/management/commands/loadcontests.py
@@ -15,38 +15,52 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('csv_filename', type=str, help='The CSV file to load data from')
 
+    def process_file(self, reader):
+        for line in reader:
+            year = line[0]
+            country = line[1]
+
+            if not (year or country):
+                self.stdout.write(self.style.WARNING(f"Data is invalid (or empty: '{year}', '{country}'). Skipped."))
+                break
+
+            # Check to see if there is a country in database with the given id.
+            if Country.objects.filter(id=country).exists():
+
+                # The year of the Contest should be unique.
+                if Contest.objects.filter(year=year).exists():
+                    self.stdout.write(self.style.WARNING(
+                        f"The Contest with the year {year} exists in database. Skipped."
+                    ))
+                else:
+                    contest = Contest(year=year, host_country=Country.objects.get(id=country))
+                    contest.save()
+            else:
+                self.stdout.write(self.style.WARNING(
+                    f"""Country with the given ID: {country} does not exists in database.
+                    First run the 'loadcountries' command or create Country with that ID manually. Skipped."""
+                ))
+
     def handle(self, *args, **options):
         csv_filename = options['csv_filename']
+
+        data = None
 
         try:
             with open(f'{csv_filename}', 'r') as csv_file:
                 csv_reader = csv.reader(csv_file)
 
-                next(csv_reader)  # The first line is the header, loop over the first line.
+                # The first line is the header, loop over the first line.
+                next(csv_reader)
+                data = list(csv_reader)
 
-                for line in csv_reader:
-                    year = line[0]
-                    country = line[1]
-
-                    # Check to see if there is a country in database with the given id.
-                    if Country.objects.filter(id=country).exists():
-
-                        # The year of the Contest should be unique.
-                        if Contest.objects.filter(year=year).exists():
-                            self.stdout.write(self.style.WARNING(
-                                f"The Contest with the year {year} exists in database. Skipped."
-                            ))
-                        else:
-                            contest = Contest(year=year, host_country=Country.objects.get(id=country))
-                            contest.save()
-                    else:
-                        self.stdout.write(self.style.WARNING(
-                            f"""Country with the given ID: {country} does not exists in database.
-                            First run the 'loadcountry' command or create Country with that ID manually. Skipped."""
-                        ))
-
-        except Exception:
+        except FileNotFoundError:
             raise CommandError(f"File '{csv_filename}' does not exist.")
 
-        self.stdout.write(self.style.SUCCESS(
-            f"Data was successfully downloaded to the Contest table in database from '{csv_filename}'."))
+        if data:
+            self.process_file(data)
+            self.stdout.write(self.style.SUCCESS(
+                f"Successfully downloaded data to the Contest table in database from '{csv_filename}'."
+            ))
+        else:
+            self.stdout.write(self.style.WARNING('The input file is empty.'))
