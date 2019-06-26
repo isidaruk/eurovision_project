0b241ea721faf1dd6ce0d1c1faee1bac1c073b9f
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-06-26 16:07:09 +0300

    Added helper function to the loadcountries command

--- countries/management/commands/loadcountries.py
+++ countries/management/commands/loadcountries.py
@@ -14,29 +14,39 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('csv_filename', type=str, help='The CSV file to load data from')
 
+    def process_file(self, reader):
+        for line in reader:
+            country_name = line[0]
+
+            # Check to see if country is already in database.
+            if Country.objects.filter(name=country_name).exists():
+                self.stdout.write(self.style.WARNING(
+                    f"Name: '{country_name}'' is already exists in the Country table in database. Skipped."
+                ))
+            else:
+                c = Country(name=country_name)
+                c.save()
+
     def handle(self, *args, **options):
         csv_filename = options['csv_filename']
+
+        data = None
 
         try:
             with open(f'{csv_filename}', 'r') as csv_file:
                 csv_reader = csv.reader(csv_file)
 
-                next(csv_reader)  # The first line is the header, loop over the first line.
+                # The first line is the header, loop over the first line.
+                next(csv_reader)
+                data = list(csv_reader)
 
-                for line in csv_reader:
-                    country_name = line[0]
-
-                    # Check to see if country is already in database.
-                    if Country.objects.filter(name=country_name).exists():
-                        self.stdout.write(self.style.WARNING(
-                            f"Some data (field Name: {country_name}) is already exists in the Country table in database. Skipped."
-                        ))
-                    else:
-                        c = Country(name=country_name)
-                        c.save()
-
-        except Exception:
+        except FileNotFoundError:
             raise CommandError(f"File '{csv_filename}' does not exist.")
 
-        self.stdout.write(self.style.SUCCESS(
-            f"Data was successfully downloaded to the Country table in database from '{csv_filename}'."))
+        if data:
+            self.process_file(data)
+            self.stdout.write(self.style.SUCCESS(
+                f"Successfully downloaded data to the Country table in database from '{csv_filename}'."
+            ))
+        else:
+            self.stdout.write(self.style.WARNING('The input file is empty.'))
