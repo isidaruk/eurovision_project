9278dfbf31cc60ce4e6a34d2a4299c761c14d042
Ivan Sidaruk <Ivan.Sidaruk@outlook.com>
2019-07-04 16:26:20 +0300

    Configured nd verified Sentry installation

--- eurovision_project/settings.py
+++ eurovision_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -224,3 +226,10 @@ if DEBUG:
     DEBUG_TOOLBAR_CONFIG = {
         'DISABLE_PANELS': ['debug_toolbar.panels.redirects.RedirectsPanel'],
     }
+
+
+# The Sentry integration
+sentry_sdk.init(
+    dsn=env('SENTRY_DSN'),
+    integrations=[DjangoIntegration()]
+)
--- eurovision_project/urls.py
+++ eurovision_project/urls.py
@@ -60,3 +60,13 @@ if settings.DEBUG:
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
+urlpatterns += [
+    path('sentry-debug/', trigger_error),
+    # ...
+]
